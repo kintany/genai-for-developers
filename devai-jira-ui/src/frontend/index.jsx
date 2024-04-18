40d9414c4ea95df4c62365d00f0aa8487468702d
kintany <kintany@gmail.com>
2024-04-18 21:43:56 +0000

    fixes

--- devai-jira-ui/src/frontend/index.jsx
+++ devai-jira-ui/src/frontend/index.jsx
@@ -61,7 +61,7 @@ const App = () => {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
       },
-      body: `{"body": "[GitLab Merge Request|https://gitlab.com/YOUR-GIT-USERID/YOUR-GIT-REPO/-/merge_requests]"}`
+      body: `{"body": "[GitLab Merge Request|https://gitlab.com/kintany/genai/-/merge_requests]"}`
     });
 
    
@@ -77,7 +77,7 @@ const App = () => {
   return (
     <>
       <Text>{description}</Text>
-      <Link href='https://gitlab.com/YOUR-GIT-USERID/YOUR-GIT-REPO/-/merge_requests' openNewTab={true}>GitLab Merge Request</Link>
+      <Link href='https://gitlab.com/kintany/genai/-/merge_requests' openNewTab={true}>GitLab Merge Request</Link>
     </>
   );
 };
